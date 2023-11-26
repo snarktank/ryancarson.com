c86e559636bc46f513a8734f69984a2425f39d18
Ryan Carson <ryan+github@ryancarson.com>
2023-11-26 10:27:50 -0500

    Add ArrowIcon component and update work page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,23 @@ import atlantic from "public/images/home/atlantic.jpg";
 import collision from "public/images/home/collision.jpg";
 import ninenineu from "public/images/home/99u.jpg";
 
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
 export default function Page() {
   return (
     <>
@@ -77,7 +94,7 @@ export default function Page() {
         <div className="relative h-40">
           <Image
             alt="My badge on top of a pile of badges from a Vercel meetup we held"
-            src={twist}
+            src={office}
             fill
             sizes="(min-width: 768px) 213px, 33vw"
             priority
@@ -85,6 +102,30 @@ export default function Page() {
           />
         </div>
       </div>
+      <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-600 dark:text-neutral-300">
+        <li>
+          <a
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://twitter.com/ryancarson"
+          >
+            <ArrowIcon />
+            <p className="h-7 ml-2">Twitter</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/ryancarson/"
+          >
+            <ArrowIcon />
+            <p className="h-7 ml-2">LinkedIn</p>
+          </a>
+        </li>
+      </ul>
     </>
   );
 }
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,69 +1,113 @@
 export default function Page() {
   return (
     <>
-      <section className="mb-10">
-        <h1 className="font-medium text-2xl mb-8 tracking-tighter">
-          Companies + Projects
-        </h1>
-        <p className="prose prose-neutral dark:prose-invert">
-          Over the past two decades, I’ve led companies as Founder & CEO and
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">My Work</h1>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          Over the past two decades, I've led companies as Founder & CEO and
           created thriving global communities.
         </p>
-      </section>
-      <section className="mb-10">
-        <h2 className="font-medium text-xl mb-8 tracking-tighter">Maple</h2>
-        <p>xxx</p>
-      </section>
-      <section className="mb-10">
-        <h2 className="font-medium text-xl mb-8 tracking-tighter">
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Maple</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Founder + Developer, 2023
+        </p>
+        <ul>
+          <li>
+            Created a custom full-stack web app for coaching and teaching Sales,
+            built on the GPT-4 Large Language Model (LLM).
+          </li>
+          <li>Created a 17-section Sales Mastery Plan to guide customers.</li>
+          <li>
+            Engineered and deployed back and front end using NextJS, TypeScript,
+            PostgreSQL, Tailwind, Clerk, Stripe, Vercel and GPT-4 Turbo.
+          </li>
+        </ul>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">
           Masterschool
         </h2>
-        <p>
-          As an Entrepreneur in Residence at Masterschool, I advised how to use
-          generative AI and Large Language Models (LLMs) to enhance online
-          career-training programs.
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Entrepreneur in Residence, 2023
         </p>
-      </section>
-      <section className="mb-10">
-        <h2 className="font-medium text-xl mb-8 tracking-tighter">Treehouse</h2>
-        <p>
-          I was the founder and CEO of Treehouse, an online technology school
-          that has taught 600,000+ students how to code. Treehouse empowers
-          students to secure high-paying jobs in tech, creating hundreds of
-          millions of dollars of generational wealth for their families. I
-          raised $23 million in investment for Treehouse. Treehouse was acquired
-          in 2021.
+        <ul>
+          <li>
+            Spearheaded Masterschool's inaugural AI LLM offering via OpenAI's
+            GPT-4 and prompt engineering, boosting student comprehension,
+            engagement, and success metrics across 3,000 enrollees.
+          </li>
+          <li>
+            Overhauled Masterschool's gateway course "Technology Foundations
+            101," crafting a new curriculum, welcome videos, and technical
+            assessments, resulting in higher student completion rates and
+            engagement.
+          </li>
+          <li>
+            Engineered an AI-driven LLM alpha product with OpenAI GPT-4 and
+            prompt engineering to autonomously evaluate and enhance student
+            progress, reducing operational costs and elevating student
+            performance.
+          </li>
+        </ul>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Treehouse</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Founder + CEO, 2010 - 2021
         </p>
-      </section>
-      <section className="mb-10">
-        <h2 className="font-medium text-xl mb-8 tracking-tighter">
+        <ul>
+          <li>
+            Founded Treehouse, raised $23M in investment, scaled the team to
+            100+, and educated 600,000+ students.
+          </li>
+          <li>
+            Led the team to build a custom online education platform with an NPS
+            of 64; enabled students to secure high-paying tech jobs and create
+            generational wealth for their families.
+          </li>
+          <li>Guided the company to acquisition in 2021.</li>
+        </ul>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">
           Carsonified
         </h2>
-        <p>
-          I was the founder and CEO of Carsonified, a stable of international
-          tech conferences and training workshops. Carsonified was known for
-          cutting-edge events across the US, UK and Europe. World-class speakers
-          included Mark Zuckerberg, Gary Vaynerchuk, Kevin Rose, Evan Williams
-          and more. Carsonified was acquired in 2011.
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Founder + CEO, 2004 - 2011
         </p>
-      </section>
-      <section className="mb-10">
-        <h2 className="font-medium text-xl mb-8 tracking-tighter">DropSend</h2>
-        <p>
-          I was the founder and CEO of DropSend, a web app that made it easy for
-          users to send large files. DropSend pioneered storing and sending
-          terabytes of data over the internet, before the genesis of cloud
-          providers like AWS and Azure. DropSend was acquired in 2008.
+        <ul>
+          <li>
+            Founded and led Carsonified, specializing in tech conferences and
+            training workshops; operated in the US, UK, and Europe.
+          </li>
+          <li>
+            Featured world-class speakers like Mark Zuckerberg, Evan Williams
+            and Gary Vaynerchuk.
+          </li>
+          <li>Guided Carsonified to acquisition in 2011.</li>
+        </ul>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">DropSend</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Founder + CEO, 2005 - 2008
         </p>
-      </section>
-      <section className="mb-10">
-        <h2 className="font-medium text-xl mb-8 tracking-tighter">BD4D</h2>
+        <ul>
+          <li>
+            Founded and led DropSend, a web app for sending large files;
+            pioneered terabytes of data transfer and storage before the advent
+            of the cloud with AWS and Azure.
+          </li>
+          <li>Guided DropSend to acquisition in 2008.</li>
+        </ul>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">BD4D</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Co-Creator, 2000 - 2004
+        </p>
         <p>
           I co-created BD4D, a global community of creatives designed to unite,
           encourage and inspire web designers and developers all over the world.
           BD4D events were held across the UK, US, Europe, Australia and Canada.
         </p>
-      </section>
+      </div>
     </>
   );
 }
