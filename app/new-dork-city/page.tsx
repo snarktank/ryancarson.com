02df5edceb6196719d2a96f277a2e1a037a4220b
Ryan Carson <ryan+github@ryancarson.com>
2023-12-29 09:08:22 -0500

    Update page layout and links

--- app/new-dork-city/page.tsx
+++ app/new-dork-city/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
   return (
     <>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
-        New Dork City
+        New Dork City Newsletter
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
-        <div className="flex">
+        <div className="flex flex-col sm:flex-row">
           <Image
             src="/images/new-dork-city/new-dork-city.jpg"
             alt="Logo of New Dork City"
@@ -16,28 +16,21 @@ export default function Page() {
             className="max-w-sm pr-4"
           />
           <p>
-            New Dork City is a podcast and newsletter for anyone interested in
-            building with AI. Beginners welcome!
+            New Dork City is a{" "}
+            <a href="https://podcasters.spotify.com/pod/show/newdorkcity">
+              podcast
+            </a>{" "}
+            and <a href="https://ryancarson.substack.com">newsletter</a> for
+            anyone interested in using and building with AI.
           </p>
+          <h2 className="text-lg">Subscribe</h2>
+          <iframe
+            src="https://ryancarson.substack.com/embed"
+            width="480"
+            height="150"
+            className="border bg-white"
+          ></iframe>
         </div>
-        <h2 className="font-medium text-xl mb-8 tracking-tighter">
-          Newsletter
-        </h2>
-        <iframe
-          src="https://ryancarson.substack.com/embed"
-          width="480"
-          height="150"
-          className="border bg-white"
-          scrolling="no"
-        ></iframe>
-
-        <h2 className="font-medium text-xl mb-8 tracking-tighter">Podcast</h2>
-        <p>
-          <a href="https://podcasters.spotify.com/pod/show/newdorkcity">
-            Subscribe on Spotify
-          </a>
-          .
-        </p>
       </div>
     </>
   );
